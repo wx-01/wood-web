93060b71c1000c7a5af272fd689f1768389908a8
Wajid Ali <94067298+wx-01@users.noreply.github.com>
2025-06-28 21:25:49 +0800

    Initial commit

---
+++ src/components/Background.jsx
@@ -0,0 +1,464 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div className="fixed inset-0 overflow-hidden pointer-events-none z-0 bg-[#222021]">
+      <div className="grid grid-cols-2 overflow-hidden">
+      <svg
+        width={1434}
+        height={1024}
+        viewBox="0 0 1434 1024"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M99.2499 0C132 25.8586 115.625 62.0605 1 77.5757"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M230.25 0C290.291 38.7878 328.5 125.673 1 162.909"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M557.75 0C530.458 67.2323 384.175 203.248 17.3751 209.454"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M803.374 0C759.708 72.404 541.375 228.072 17.3751 271.515"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1098.12 0C934.374 87.9191 583.95 263.757 492.25 263.757C400.55 263.757 126.542 413.737 1 488.727"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 0C606.875 235.313 -696.574 769.551 639.625 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1 915.392C28.2916 949.008 112.35 1017.79 230.25 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 93.0914C754.251 307.717 -211.874 681.115 1425.63 457.697"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.63 170.668C1038.08 292.203 344.875 574.061 1425.63 395.637"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 511.999C765.166 548.201 -15.3746 713.696 852.499 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 605.091C1016.25 628.363 213.875 651.636 950.75 1016.24"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 698.182C1109.04 705.94 492.249 659.394 1049 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 767.999C1218.21 786.1 786.999 705.939 1163.62 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 847.196C1303.22 861.228 1043 754.585 1268.52 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1392.87 1024H1425.62"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+      </svg>
+      <svg
+        width={1434}
+        height={1024}
+        viewBox="0 0 1434 1024"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M99.2499 0C132 25.8586 115.625 62.0605 1 77.5757"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M230.25 0C290.291 38.7878 328.5 125.673 1 162.909"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M557.75 0C530.458 67.2323 384.175 203.248 17.3751 209.454"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M803.374 0C759.708 72.404 541.375 228.072 17.3751 271.515"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1098.12 0C934.374 87.9191 583.95 263.757 492.25 263.757C400.55 263.757 126.542 413.737 1 488.727"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 0C606.875 235.313 -696.574 769.551 639.625 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1 915.392C28.2916 949.008 112.35 1017.79 230.25 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 93.0914C754.251 307.717 -211.874 681.115 1425.63 457.697"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.63 170.668C1038.08 292.203 344.875 574.061 1425.63 395.637"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 511.999C765.166 548.201 -15.3746 713.696 852.499 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 605.091C1016.25 628.363 213.875 651.636 950.75 1016.24"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 698.182C1109.04 705.94 492.249 659.394 1049 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 767.999C1218.21 786.1 786.999 705.939 1163.62 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 847.196C1303.22 861.228 1043 754.585 1268.52 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1392.87 1024H1425.62"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+      </svg>
+      <svg
+        width={1434}
+        height={1024}
+        viewBox="0 0 1434 1024"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M99.2499 0C132 25.8586 115.625 62.0605 1 77.5757"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M230.25 0C290.291 38.7878 328.5 125.673 1 162.909"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M557.75 0C530.458 67.2323 384.175 203.248 17.3751 209.454"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M803.374 0C759.708 72.404 541.375 228.072 17.3751 271.515"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1098.12 0C934.374 87.9191 583.95 263.757 492.25 263.757C400.55 263.757 126.542 413.737 1 488.727"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 0C606.875 235.313 -696.574 769.551 639.625 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1 915.392C28.2916 949.008 112.35 1017.79 230.25 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 93.0914C754.251 307.717 -211.874 681.115 1425.63 457.697"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.63 170.668C1038.08 292.203 344.875 574.061 1425.63 395.637"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 511.999C765.166 548.201 -15.3746 713.696 852.499 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 605.091C1016.25 628.363 213.875 651.636 950.75 1016.24"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 698.182C1109.04 705.94 492.249 659.394 1049 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 767.999C1218.21 786.1 786.999 705.939 1163.62 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 847.196C1303.22 861.228 1043 754.585 1268.52 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1392.87 1024H1425.62"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+      </svg>
+      <svg
+        width={1434}
+        height={1024}
+        viewBox="0 0 1434 1024"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M99.2499 0C132 25.8586 115.625 62.0605 1 77.5757"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M230.25 0C290.291 38.7878 328.5 125.673 1 162.909"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M557.75 0C530.458 67.2323 384.175 203.248 17.3751 209.454"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M803.374 0C759.708 72.404 541.375 228.072 17.3751 271.515"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1098.12 0C934.374 87.9191 583.95 263.757 492.25 263.757C400.55 263.757 126.542 413.737 1 488.727"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 0C606.875 235.313 -696.574 769.551 639.625 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1 915.392C28.2916 949.008 112.35 1017.79 230.25 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 93.0914C754.251 307.717 -211.874 681.115 1425.63 457.697"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.63 170.668C1038.08 292.203 344.875 574.061 1425.63 395.637"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 511.999C765.166 548.201 -15.3746 713.696 852.499 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 605.091C1016.25 628.363 213.875 651.636 950.75 1016.24"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 698.182C1109.04 705.94 492.249 659.394 1049 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1425.62 767.999C1218.21 786.1 786.999 705.939 1163.62 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1442 847.196C1303.22 861.228 1043 754.585 1268.52 1024"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+        <path
+          d="M1392.87 1024H1425.62"
+          stroke="#F1DED0"
+          strokeOpacity={0.29}
+          strokeWidth={0.5}
+          strokeLinecap="round"
+        />
+      </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Background;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Background from '../components/Background'
+
+const Home = () => {
+  return (
+    <div >
+      Home
+    </div>
+  )
+}
+
+export default Home
