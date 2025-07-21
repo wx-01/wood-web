3291d013d3a220e7b48dffbbef461d44dfc52b67
Syeda Qandeel Fakhar <sqandeelfakhar@gmail.com>
2025-07-22 00:23:37 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Background from '../components/Background'
+import React from "react";
+import Background from "../components/Background";
 
 const Home = () => {
   return (
-    <div >
-      Home
-    </div>
-  )
-}
+    <section className="min-h-screen bg-white flex items-center justify-center text-center">
+      <div>
+        <h1 className="text-5xl font-bold text-gray-800 mb-6">
+          Home Component Loaded
+        </h1>
+        <p className="text-lg text-gray-600">
+          This is the hero section of wood product website.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
