102dd72e70a55b95b8e9f09f5aa8b2b740292a84
Wajid Ali <94067298+wx-01@users.noreply.github.com>
2025-07-22 11:18:36 +0500

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Background from "../components/Background";
 
 const Home = () => {
   return (
-    <section className="min-h-screen bg-white flex items-center justify-center text-center">
+    <section className="relative min-h-[10px] mt-50 bg-white flex items-center justify-center text-center z-30">
       <div>
         <h1 className="text-5xl font-bold text-gray-800 mb-6">
           Home Component Loaded
